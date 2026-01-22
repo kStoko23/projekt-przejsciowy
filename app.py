9a2168c7c85ece99b8a16d24131f83f4e9e1c689
Kuba Stokowski <j.stokowskii23@gmail.com>
2026-01-22 11:54:56 +0100

    Added more plots

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ age_range = st.sidebar.slider(
     "Zakres wieku",
     int(df["age"].min()),
     int(df["age"].max()),
-    (10, 60)
+    (0, int(df["age"].max()))
 )
 
 filtered_df = df[
@@ -36,7 +36,7 @@ filtered_df = df[
     (df["pclass"].isin(pclass)) &
     (df["age"].between(age_range[0], age_range[1]))
 ]
-
+st.divider()
 st.subheader("Dane po filtracji")
 st.write(f"Liczba obserwacji: {len(filtered_df)}")
 st.dataframe(filtered_df)
@@ -57,8 +57,41 @@ fig = px.bar(
     title="Przeżywalność pasażerów",
     text="Liczba"
 )
-
+st.divider()
 st.plotly_chart(fig)
+st.divider()
 
-st.subheader("Podstawowe statystyki wieku")
-st.write(filtered_df["age"].describe())
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Podstawowe statystyki dla grupy kobiet")
+    st.write(filtered_df[filtered_df["sex"] == "female"]["age"].describe())
+
+with col2:
+    st.subheader("Podstawowe statystyki dla grupy mężczyzn")
+    st.write(filtered_df[filtered_df["sex"] == "male"]["age"].describe())
+
+
+st.divider()
+
+sex_survival = (
+    df
+    .groupby(["sex", "survived"])
+    .size()
+    .reset_index(name="Liczba")
+)
+
+sex_survival["Płeć"] = sex_survival["sex"].map({"female": "Kobiety", "male": "Mężczyźni"})
+sex_survival["Status"] = sex_survival["survived"].map({0: "Nie przeżył", 1: "Przeżył"})
+
+fig_sex = px.bar(
+    sex_survival,
+    x="Płeć",
+    y="Liczba",
+    color="Status",
+    barmode="stack",
+    text="Liczba",
+    title="Przeżywalność pasażerów wg płci"
+)
+
+st.plotly_chart(fig_sex)
+
